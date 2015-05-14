16b8477ad512c9b31953ffcc0cb2996606bf634d
Joe Turgeon <jturgeon@phase2technology.com>
2015-05-14 14:01:28 -0700

    Initial commit of the p2 generator.

---
+++ app/index.js
@@ -0,0 +1,75 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the fantabulous ' + chalk.red('Phase2') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'themeName',
+      message: 'What would you like to name the theme?',
+      default: ''
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.themeName = props.themeName;
+      this.themePath = 'src/themes/' + this.themeName;
+
+      this.composeWith('gadget', { options: {
+        themeName: this.themeName,
+        themePath: this.themePath
+      }});
+
+      this.composeWith('pattern-lab-starter', { options: {
+        themeName: this.themeName,
+        themePath: this.themePath
+      }});
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+/*
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+*/
+  },
+
+  install: function () {
+//    this.installDependencies({
+//      skipInstall: this.options['skip-install']
+//    });
+  }
+});
