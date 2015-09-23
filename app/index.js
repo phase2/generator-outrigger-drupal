c2652e9b6468d65ff3ff9f0da0dcf77a2842cc4d
Evan Lovely <evan@evanlovely.com>
2015-09-23 14:00:12 -0700

    improving options approach

--- app/index.js
+++ app/index.js
@@ -2,44 +2,53 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
+var options = {};
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the fantabulous ' + chalk.red('Phase2') + ' generator!'
+    ));
+    options = _.assign(this.config, this.options);
   },
 
   prompting: function () {
     var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fantabulous ' + chalk.red('Phase2') + ' generator!'
-    ));
 
-    var prompts = [{
-      type: 'input',
-      name: 'themeName',
-      message: 'What would you like to name the theme?',
-      default: ''
-    }];
+    var prompts = [];
+
+    if (!options.themeName) {
+      prompts.push({
+        type: 'input',
+        name: 'themeName',
+        message: 'What would you like to name the theme?',
+        default: ''
+      });
+    }
 
     this.prompt(prompts, function (props) {
-      this.themeName = props.themeName;
-      this.themePath = 'src/themes/' + this.themeName;
+      options = _.assign(options, props);
+      //this.themeName = props.themeName;
+      options.themePath = 'src/themes/' + options.themeName;
 
       this.composeWith('gadget', { options: {
         skipWelcome: true,
-        themeName: this.themeName,
-        themePath: this.themePath,
+        themeName: options.themeName,
+        themePath: options.themePath,
         themeScripts: {
-          "compile-theme": "npm run compile"
+          "compile-theme": "npm run compile",
+          "validate": "npm run test"
         }
       }});
 
       this.composeWith('pattern-lab-starter', { options: {
         skipWelcome: true,
-        themeName: this.themeName,
-        themePath: this.themePath
+        themeName: options.themeName,
+        themePath: options.themePath
       }});
 
       done();
