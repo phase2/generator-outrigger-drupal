e39f670a690a851081d5a582e7096722ce3cf03e
Grayside <aross@phase2technology.com>
2016-01-29 13:50:39 -0800

    Make better use of error handling to halt and clean up after jobs.

--- templates/bin/start.sh
+++ templates/bin/start.sh
@@ -14,6 +14,10 @@
 #    project specific name for docker-compose.
 ##
 
+##
+# Initialize parameters.
+##
+NAME=`basename "$0"`
 DOCKER_ENV=$1
 COMPOSE_EXT=".$1"
 if [[ -z $DOCKER_ENV ]]; then
@@ -30,14 +34,35 @@ if [[ $DOCKER_ENV == 'local' ]]; then
   COMPOSE_PROJECT=''
 fi
 
+##
+# Error handling.
+##
+
+trap '
+  ret=$?
+  [ "$ret" -eq 0 ] || echo >&2 "$NAME aborted: $ret"' EXIT
+
+# Cancel docker start on errors.
+trap 'cancel' ERR
+
+cancel() {
+  # Stop all containers. This will allow follow-up runs via Jenkins to manage
+  # the workspace and containers since some preparation operations are compromised
+  # by the www container.
+  docker-compose -f docker-compose$COMPOSE_EXT.yml ${COMPOSE_PROJECT} stop
+  exit 33
+}
+
 # Spin up cache and db services to support build container.
 # Web container might take file locks on existing code, blocking the build process.
 docker-compose -f docker-compose$COMPOSE_EXT.yml ${COMPOSE_PROJECT} up -d <% if(cacheExternal) { %>cache <% } %>db
 
 # Build and run static analysis.
+# Run grunt with --force to ignore errors.
 docker-compose -f build$COMPOSE_EXT.yml ${COMPOSE_PROJECT} run --rm cli sh -c "\
-npm install && \
-grunt --force"
+# --unsafe-perm ensures dispatch to theme-related operations can still run as root for Docker.
+npm install --unsafe-perm && \
+grunt --timer --quiet"
 
 # Now safe to activate web container to support end-to-end testing.
 docker-compose -f docker-compose$COMPOSE_EXT.yml ${COMPOSE_PROJECT} up -d www
@@ -50,7 +75,6 @@ chmod +x /var/www/bin/fix-perms.sh && \
 # Install the site.
 docker-compose -f build$COMPOSE_EXT.yml ${COMPOSE_PROJECT} run --rm cli sh -c "\
 # Errors in final steps of installation require --force to ensure bin/post-install.sh is run.
-# Dev triggers a development build of Open Atrium
 grunt install --no-db-load --force"
 
 # Wipe cache after permissions fix.
