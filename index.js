1822272de9b1b5960b9b3fc23edff042d6f2b763
Grayside <aross@phase2technology.com>
2016-02-21 15:33:34 -0800

    Minor corrections and addition of deploy-local job to facilitate troubleshooting.

--- index.js
+++ index.js
@@ -88,7 +88,8 @@ module.exports = yeoman.generators.Base.extend({
         int: virtualHost('int', options.machineName),
         dev: virtualHost('dev', options.machineName),
         qa: virtualHost('qa', options.machineName),
-        ms: virtualHost(false, options.machineName)
+        ms: virtualHost(false, options.machineName),
+        local: 'www.' + options.domain + '.vm',
       };
 
       done();
@@ -343,6 +344,16 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copyTpl(
         this.templatePath('jenkins/JENKINS.md'),
         this.destinationPath('JENKINS.md'),
+        tokens
+      );
+
+      // Add local environment to facilitate testing.
+      tokens.virtualHost = tokens.host.local;
+      tokens.environment = 0;
+      tokens.dockerComposeExt = '';
+      this.fs.copyTpl(
+        this.templatePath('jenkins/jobs-optional/deploy-env'),
+        this.destinationPath('env/jenkins/jobs/deploy-local'),
         tokens
       );
 
