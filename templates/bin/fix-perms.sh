98b5c38c10fc9c7c4592273f2bf7cb2126c45c7a
Chris Johnson <git@tekante.com>
2016-06-26 22:44:55 -0400

    Updated commentary about files directory path significance and ensured consistency between them.

--- templates/bin/fix-perms.sh
+++ templates/bin/fix-perms.sh
@@ -4,9 +4,12 @@
 #
 # Some of the file permissions are broken or mis-aligned as a result
 # of the various Docker and filesystem layers.
+#
+# Paths used align with the volume mappings in the docker-compose
+# and build configuration files.
 ##
 
 set -x
-mkdir -p /var/www/build/html/sites/default/files/private
-chown -R apache:apache /var/www/build/html/sites/default/files
-chmod 755 /var/www/build/html/sites/default/files
+mkdir -p /var/www/src/sites/default/files/private
+chown -R apache:apache /var/www/src/sites/default/files
+chmod 755 /var/www/src/sites/default/files
