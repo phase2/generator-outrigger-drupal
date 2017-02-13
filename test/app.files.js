6b7f277eb8df41edfa07c6985acf99308cc2146b
Grayside <aross@phase2technology.com>
2017-02-12 23:34:56 -0800

    Fix the tests for new p2-env:jenkins

---
+++ test/app.files.js
@@ -0,0 +1,39 @@
+var jenkins = require('./jenkins.files.js');
+
+module.exports = {
+  minimum: [
+    'bin/db-export.sh',
+    'bin/features-health.sh',
+    'bin/fix-perms.sh',
+    'bin/framework.sh',
+    'bin/pre-install.sh',
+    'bin/seed-users.sh',
+    'bin/start.sh',
+    'bin/update-scanner.sh',
+    'build.yml',
+    'build.devcloud.yml',
+    'CONTRIBUTING.md',
+    'DOCKER.md',
+    'docker-compose.yml',
+    'docker-compose.devcloud.yml',
+    'env/build/etc/drush/drushrc.php',
+    'env/build/etc/drush/aliases.drushrc.php',
+    'Gruntconfig.json',
+    'package.json',
+    'src/sites/settings.common.php',
+    'TODOS.md'
+  ],
+  extended: [],
+  minimumJenkins: jenkins.minimum,
+  extendedJenkins: jenkins.extended,
+  json: {
+    'Gruntconfig.json': 'Gruntconfig.json',
+    'package.json': 'package.json'
+  },
+  yaml: {
+    'docker-compose.yml': 'docker-compose.yml',
+    'docker-compose.devcloud.yml': 'docker-compose.devcloud.yml',
+    'build.yml': 'build.yml',
+    'build.devcloud.yml': 'build.devcloud.yml'
+  }
+};
