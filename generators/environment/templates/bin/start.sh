6b95254907b86ef61cf0daae5f6c0f1ac0b3921d
Adam Ross <aross@phase2technology.com>
2017-09-05 16:57:29 -0700

    Fix formatting of deprecation warning in start.sh

--- generators/environment/templates/bin/start.sh
+++ generators/environment/templates/bin/start.sh
@@ -9,6 +9,6 @@
 CALLPATH=`dirname $0`
 source "$CALLPATH/framework.sh"
 
-echoWarn "Outrigger Drupal v4 dropped support for the start.sh script utility."
-echoWarn "For local development, replace your daily operations with rig project commands."
-echoError "Jenkins jobs should re-generated or refactored to use the individual steps."
+echoWarn "Outrigger Drupal v4 dropped support for the start.sh script utility.\n"
+echoWarn "For local development, replace your daily operations with rig project commands.\n"
+echoError "Jenkins jobs should re-generated or refactored to use the individual steps.\n"
